573b2303d12c2691181dcc67a12f473e4c86521a
vardhanak <achantakrishnavardhan@gmail.com>
2022-02-20 20:08:20 +0530

    weather

--- weather.js
+++ weather.js
@@ -1,21 +1,23 @@
-//Complete the Weather API Backend part using openweathermap api
-var apikey = 'f89858816e82a3f9f7b5a9f1a8d37cc9';
-var msg = document.getElementById("msg");
+let day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-function getData(){
-    var input = document.getElementById("city").value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q={input}&appid={apikey}@units=metric`;
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
+document.querySelector('.search').addEventListener(('click'),()=>{
+    let name=document.querySelector('.search-box').value;
+    if(name===""){
+        alert("please enter the city!");
+        return;
+    }fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=f89858816e82a3f9f7b5a9f1a8d37cc9`)
+    .then((response) => response.json())
+    .then((data) => {
         console.log(data);
-        document.getElementById("p1").innerHTML = "Temp: "+data.main.temp;
-        document.getElementById("p2").innerHTML = "Temp: "+data.name;
-        var icon = "https://api.openweather.org/img/w"+data.weather[0].icon+".png";
-        document.getElementById("icon").src = icon;
+        document.querySelector('.city').textContent = `${data.name}, ${data.sys.country}`;
+        let date = new Date();
+        document.querySelector('.date').textContent = `${day[date.getDay()]} ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`;
+        document.querySelector('.temp').textContent = `${parseInt(data.main.temp-273.15)}°c`;
+        document.querySelector('.weather').textContent = `${data.weather[0].main}`;
+        document.querySelector('.hi-low').textContent = `${parseInt(data.main.temp_min-273.15)}°c / ${parseInt(data.main.temp_max-273.15)}°c`;
     })
     .catch(() => {
-        msg.textcontent = "please search for a valid city"
+        alert("The city you entered is incorrect!");
     })
-}
+});
